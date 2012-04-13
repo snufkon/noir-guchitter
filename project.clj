b00a756abe1d7465bd2848913c2c456f7106298b
snufkon <snufkon@gmail.com>
2012-04-14 06:24:31 +0900

    project.clj の description を修正。

--- project.clj
+++ project.clj
@@ -1,9 +1,8 @@
-(defproject noir-guchitter "0.1.0-SNAPSHOT"
-  :description "FIXME: write this!"
+(defproject noir-guchitter "1.0.0-SNAPSHOT"
+  :description "noir-guchitter"
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [noir "1.2.1"]
                  [postgresql "9.1-901.jdbc4"]
-                 [org.clojure/java.jdbc "0.1.1"]
-                 ]
+                 [org.clojure/java.jdbc "0.1.1"]]
   :main noir-guchitter.server)
 
